1c61fc98a650cbb5803aa0ac3ea2832678c44db2
ekg555 <eugene.konagaya@gmail.com>
2019-05-21 07:58:57 -0700

    Add files via upload

---
+++ Rescue.R
@@ -0,0 +1,16 @@
+#	Rescue.R
+# ========================================================================
+#   Sister Script of Searche.R
+# ------------------------------------------------------------------------
+# After doing any pre-processing to the PATH & Dates sheet...
+# COPY ALL PATHS of FILES to RESCUE in CLIPBOARD...then
+dsktp <- 'c:/users/ekonagaya/desktop'
+wkdir <- file.path(dsktp, "MIA PERMITs")
+
+if (!dir.exists(wkdir)) { dir.create(wkdir) }
+
+x <- readClipboard()
+x <- gsub("\\\\", "/", x)
+x <- x[-1]	# HEADER
+
+file.copy(x[1],wkdir)
