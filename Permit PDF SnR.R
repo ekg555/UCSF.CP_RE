76eb7674a4122fd3643159d88b0669f1a4fa69da
ekg555 <eugene.konagaya@gmail.com>
2019-05-22 14:42:59 -0700

    Add files via upload

--- Permit PDF SnR.R
+++ Permit PDF SnR.R
@@ -2,12 +2,43 @@ rm(list=ls())
 
 library(readxl)
 library(stringr)
+library(beepr)
 dsktp <- 'c:/users/ekonagaya/desktop'
+# ----------------------------------------------------------------------------
+wkdir <- dsktp
+srcdir <- 'p:'
+# ============================================================================
+T1.name <- 'Permit'
+T1.regx <- '([\\\\])I[0-9]{3,}[[:space:]]'
 
- timestp <- format(Sys.time(), "%Y-%m-%d.%H") # Time Stamp as Ousual
- 
-setwd(dsktp)
-PATHs <- readLines('The_Lost_Permits.txt')
+T2.name <- '100CD'
+T1.regx <- '100[\\s]{0,}%?[\\s]{0,}CD'
+
+# ============================================================================
+#  CREATE The MANIFEST (Win10)
+# ============================================================================
+setwd(wkdir)
+if (!file.exists("PDF_Manifest.txt")) {
+setwd(srcdir)
+
+system("cmd", 
+		intern=T, 
+		ignore.stdout=F, 
+		ignore.stderr=F, 
+		wait=T, 
+		input='dir /B /S | findstr("pdf$") > PDF_Manifest.txt')
+				
+file.copy('PDF_Manifest.txt', file.path(wkdir, 'PDF_Manifest.txt'))
+beep(2)
+Sys.sleep(3)
+file.remove('PDF_Manifest.txt')
+}
+
+setwd(wkdir)
+
+# ============================================================================		
+	
+PATHs <- readLines('PDF_Manifest.txt')
 PNums <- trimws(gsub("\\", "", unlist(str_extract_all(string=PATHs, pattern="([\\\\])I[0-9]{3,}[[:space:]]",simplify=T)), fixed=T))
 
 dat1 <- data.frame(PATHs, PNums, stringsAsFactors=F)
@@ -16,10 +47,9 @@ PDATES <- read_excel('COMPILED PERMIT DATES 05-20.xlsx')
 In_PERMITS <- setdiff(PDATES$"PERMIT .", PNums)	
 MIA <- setdiff(PNums, PDATES$"PERMIT .")			# MIA: Permit# in 001, but Not in 004
 
-
 paste0(MIA, collapse="|")
  grep(paste0("[",paste0(MIA, collapse="|"),"]"), PATHs, val=T)
  
 MIA <- setdiff(PNums, PDATES$"PERMIT .") 	# The MIA PERMITS (MAY or MAY NOT Be SIGNED)
 nrow(dat1[which(dat1$PNums %in% MIA),]) # n= 60 files w/ MIA-Permit#s
-write.csv(dat1[which(dat1$PNums %in% MIA),], paste0('MIA-PERMITS_',timestp,'.csv'))
+write.csv(dat1[which(dat1$PNums %in% MIA),], 'MIA-PERMITS_')
